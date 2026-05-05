5fda1dee144ee40e0c42cfcf11fe753c6a0547e6
unknown <prashanthsolur112@gmail.com>
2026-05-05 17:59:40 +0530

    daytwo

---
+++ daytwo/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function App() {
+  const yesterday = () => {
+    const d = new Date();
+    d.setDate(d.getDate() - 1);
+    return d;
+  };
+  const [date, setDate] = useState(yesterday());
+
+  return (
+    <div>
+      <label>Basic date</label>
+      <DatePicker
+        selected={date}
+        onChange={(date) => setDate(date)}
+        dateFormat="yyyy-MM-dd"
+      />
+    </div>
+  );
+}
